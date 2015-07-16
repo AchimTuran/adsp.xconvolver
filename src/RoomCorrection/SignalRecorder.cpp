e5bae1cc3a4ff3cec10dca559f82c01b78b8e2ef
Achim Turan <mail@achim-turan.de>
2015-07-16 08:47:08 +0200

    [SignalRecorder] Improved functionality with: - extra thread and an state machine - multichannel recording - prefix and postfix filenames - added a define for the processing frame length

--- src/RoomCorrection/SignalRecorder.cpp
+++ src/RoomCorrection/SignalRecorder.cpp
@@ -2,107 +2,134 @@
 
 #include <template/include/typedefs.h>
 #include "utils/stdStringUtils.h"
-#include <sndfile/sndfile.hh>
+#include <asplib/asplib_utils/signals/interleaver.h>
 
 #include <iostream>
 
 using namespace ADDON;
 using namespace std;
+using namespace asplib;
+using namespace PLATFORM;
 
-CSignalRecorder::CSignalRecorder()
+CSignalRecorder::CSignalRecorder(string PrefixStr)
 {
-  m_bStop = true;
-
+  m_PrefixStr = PrefixStr;
   asplib::CPaHostAPIVector_t usedHostAPIs;
   //usedHostAPIs.push_back(paASIO);
   //usedHostAPIs.push_back(paMME);
   //usedHostAPIs.push_back(paWASAPI);
   //usedHostAPIs.push_back(paDirectSound);
   m_CaptureDevice = new PortAudioSource(usedHostAPIs);
+  m_SamplesBuffer = NULL;
+  m_CaptureWave   = NULL;
+  m_FrameSize     = DEFAULT_FRAME_SIZE;
+  Set_State(STATE_INVALID);
 }
 
 CSignalRecorder::~CSignalRecorder()
 {
   Destroy();
-}
-
-bool CSignalRecorder::Create(uint SampleFrequency, string CaptureDevice)
-{
-  if(IsRunning())
-  {
-    StopRecording();
-    while (IsRunning());
-  }
-
-  if (!KODI->DirectoryExists(generateFilePath(g_strUserPath, "measurements").c_str()))
-  {
-    KODI->CreateDirectory(generateFilePath(g_strUserPath, "measurements").c_str());
-  }
-
-  memset(m_Samples, 0, sizeof(float) * 2048);
-
-  if (!m_CaptureDevice->Create(SampleFrequency, 2048, CaptureDevice))
-  {
-    KODI->Log(LOG_ERROR, "Couldn't create capture device!");
-    return false;
-  }
-
-  m_CaptureDevice->get_InputFrameSize();
-
-  if (CThread::IsRunning())
-  {
-    CThread::StopThread(100);
-    m_bStop = true;
-    while (!CThread::IsStopped());
-  }
-  if (!CThread::CreateThread())
-  {
-    KODI->Log(LOG_ERROR, "Couldn't create playing thread!");
-    return false;
-  }
-
-  return true;
-}
-
-bool CSignalRecorder::Destroy()
-{
-  // wait 500ms to stop the playing thread
-  CThread::StopThread(-1);
 
   if(m_CaptureDevice)
   {
     delete m_CaptureDevice;
     m_CaptureDevice = NULL;
   }
+}
+
+bool CSignalRecorder::Create(uint SampleFrequency, uint MaxCaptureChannels, string CaptureDevice)
+{
+  // if there is already a running thread close it and delete all buffers
+  Destroy();
+
+  string capturePath = generateFilePath(g_strUserPath, "measurements");
+  if(!KODI->DirectoryExists(capturePath.c_str()))
+  {
+    KODI->CreateDirectory(capturePath.c_str());
+  }
+
+  if(!m_CaptureDevice->Create(SampleFrequency, DEFAULT_FRAME_SIZE, MaxCaptureChannels, CaptureDevice))
+  {
+    KODI->Log(LOG_ERROR, "Couldn't create capture device!");
+    return false;
+  }
+
+  m_FrameSize = m_CaptureDevice->get_InputFrameSize();
+  m_MaxCaptureChannels = m_CaptureDevice->get_InputChannelAmount();
+  if(m_MaxCaptureChannels <= 0 || m_FrameSize <= 0)
+  {
+    KODI->Log(LOG_ERROR, "Invalid FrameSize or CaptureChannel amount!");
+    return false;
+  }
+  m_SamplesBuffer = new CFloatFrameBuffer(m_FrameSize, m_MaxCaptureChannels);
+
+  Set_State(STATE_INVALID);
+  if (!CThread::CreateThread())
+  {
+    KODI->Log(LOG_ERROR, "Couldn't create playing thread!");
+    return false;
+  }
+  Set_State(STATE_IDLE);
 
   return true;
 }
 
-bool CSignalRecorder::StartRecording()
+bool CSignalRecorder::Destroy()
 {
-  if (m_bStop)
+  if(CThread::IsRunning())
   {
-    m_bStop = false;
+    // imidiatly stop thread
+    CThread::StopThread(-1);
   }
-  else
+
+  if(m_SamplesBuffer)
+  {
+    delete m_SamplesBuffer;
+    m_SamplesBuffer = NULL;
+  }
+
+  if(m_CaptureWave)
+  {
+    delete m_CaptureWave;
+    m_CaptureWave = NULL;
+  }
+
+  return true;
+}
+
+bool CSignalRecorder::StartRecording(string PostFixStr)
+{
+  if(!CThread::IsRunning())
   {
     return false;
   }
+
+  m_PostFixStr = PostFixStr;
+  Set_State(STATE_NEW_FILE);
 
   return true;
 }
 
 bool CSignalRecorder::StopRecording()
 {
-  if (!m_bStop)
-  {
-    m_bStop = true;
-    bool ret = CThread::StopThread(100);
-  }
-  else
+  if(!CThread::IsRunning())
   {
     return false;
   }
+
+  Set_State(STATE_STOP);
+
+  return true;
+}
+
+bool CSignalRecorder::FinishRecording()
+{
+  if(!CThread::IsRunning())
+  {
+    return false;
+  }
+
+  Set_State(STATE_FINISH_RECORDING);
 
   return true;
 }
@@ -119,57 +146,161 @@ unsigned int CSignalRecorder::Get_AvailableDevices(CCaptureDeviceList_t &DeviceL
   return m_CaptureDevice->Get_Devices(DeviceList, SampleFrequencies);
 }
 
+CSignalRecorder::RecorderState CSignalRecorder::Get_CurrentState()
+{
+  CLockObject lock(m_CriticalState);
+  
+  return m_State;
+}
+
+void CSignalRecorder::Set_State(RecorderState State)
+{
+  CLockObject lock(m_CriticalState);
+
+  m_State = State;
+}
+
 void *CSignalRecorder::Process(void)
 {
-  if (!m_CaptureDevice)
+  try
   {
-    KODI->Log(LOG_ERROR, "%s: called with no audio stream!", __func__);
-    return NULL;
-  }
-
-  string wavFilename = generateFilePath(generateFilePath(g_strUserPath, "measurements"), "capture.wav");
-  SndfileHandle captureWave(wavFilename.c_str(), SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_FLOAT, 1, m_CaptureDevice->get_InputSampleFrequency());
-  if (captureWave.error() != SF_ERR_NO_ERROR)
-  {
-    KODI->Log(LOG_ERROR, "Couldn't create capture wave file! libsndfile error: %s", sf_error_number(captureWave.error()));
-    return NULL;
-  }
-
-  // wait until StartRecording is called
-  while (m_bStop);
-  KODI->Log(LOG_DEBUG, "Starting measurement process.");
-  m_CaptureDevice->StartCapturing();
-
-  // record until StopRecording(...) is called
-  while (!m_bStop)
-  {
-    ulong capturedSamples = 0;
-    do
+    if(!m_CaptureDevice)
     {
-      capturedSamples = m_CaptureDevice->GetSamples(m_Samples, 2048);
-      if (capturedSamples > 0)
+      KODI->Log(LOG_ERROR, "%s: called with no audio stream!", __func__);
+      return NULL;
+    }
+    string waveFilename;
+    string wavFilePath;
+    RecorderState recState = STATE_INVALID;
+
+    while(Get_CurrentState() == STATE_INVALID);
+    recState = Get_CurrentState();
+
+    KODI->Log(LOG_DEBUG, "Starting signal recorder.");
+
+    // record until StopRecording(...) is called
+    while(recState > STATE_INVALID && recState < STATE_MAX && recState != STATE_FINISH_RECORDING)
+    {
+      switch(recState)
       {
-        ulong writtenSamples = (ulong)captureWave.write(m_Samples, capturedSamples);
-        if (writtenSamples < capturedSamples)
-        {
-          m_CaptureDevice->Destroy();
-          KODI->Log(LOG_ERROR, "Failed to write to capture wave file!");
-          return NULL;
-        }
-      }
-    } while (capturedSamples > 0);
+        case STATE_IDLE:
+        break;
 
-    if (m_bStop)
-    {
-      break;
+        case STATE_CAPTURING:
+          StoreCapturedSamples(*m_CaptureWave);
+        break;
+
+        case STATE_NEW_FILE:
+          if(m_CaptureWave)
+          {
+            if(m_CaptureDevice->GetStoredSamples() > 0)
+            {
+              StoreCapturedSamples(*m_CaptureWave);
+            }
+
+            delete m_CaptureWave;
+            m_CaptureWave = NULL;
+          }
+
+          waveFilename = m_PrefixStr + m_PostFixStr + string(".wav");
+          wavFilePath = generateFilePath(generateFilePath(g_strUserPath, "measurements"), waveFilename);
+          m_CaptureWave = new SndfileHandle(wavFilePath.c_str(), SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_FLOAT, m_MaxCaptureChannels, (int)m_CaptureDevice->get_InputSampleFrequency());
+          if(m_CaptureWave->error() != SF_ERR_NO_ERROR)
+          {
+            KODI->Log(LOG_ERROR, "Couldn't create capture wave file! libsndfile error: %s", sf_error_number(m_CaptureWave->error()));
+            return NULL;
+          }
+
+          if(!m_CaptureDevice->StartCapturing())
+          {
+            KODI->Log(LOG_ERROR, "Failed to start capturing device! Aborting...");
+            Set_State(STATE_INVALID);
+          }
+
+          Set_State(STATE_CAPTURING);
+        break;
+
+        case STATE_STOP:
+          // save last captured samples
+          if(m_CaptureDevice->GetStoredSamples() > 0)
+          {
+            StoreCapturedSamples(*m_CaptureWave);
+          }
+
+          if(!m_CaptureDevice->StopCapturing())
+          {
+            KODI->Log(LOG_ERROR, "Failed to stop capturing device! Aborting...");
+            Set_State(STATE_INVALID);
+          }
+
+          Set_State(STATE_IDLE);
+        break;
+
+        default:
+        {
+          KODI->Log(LOG_ERROR, "Invalid signal recorder state! Aborting measurement!");
+          Set_State(STATE_INVALID);
+        }
+        break;
+      }
+
+      // Update current signal recorder state
+      recState = Get_CurrentState();
+
+      if(recState == STATE_CAPTURING)
+      {
+        CThread::Sleep((uint32_t)((double)(m_CaptureDevice->get_InputFrameSize()*2.0*1000.0))/((double)m_CaptureDevice->get_InputSampleFrequency()));
+      }
     }
 
-    CThread::Sleep((uint32_t)((double)(m_CaptureDevice->get_InputFrameSize()*2*1000.0))/((double)m_CaptureDevice->get_InputSampleFrequency()));
+    m_CaptureDevice->StopCapturing();
+    if(m_CaptureWave && m_CaptureDevice->GetStoredSamples() > 0)
+    {
+      StoreCapturedSamples(*m_CaptureWave);
+    }
+    m_CaptureDevice->Destroy();
+
+    Set_State(STATE_INVALID);
+
+    return NULL;
+  }
+  catch(bad_alloc &e)
+  {
+    KODI->Log(LOG_ERROR, "%s: catched exception: %s", __func__, e.what());
+  }
+  catch(...)
+  {
+    KODI->Log(LOG_ERROR, "%s: catched unhandled exception: %s", __func__);
   }
 
-  CThread::Sleep((uint32_t)((double)(m_CaptureDevice->get_InputFrameSize()*2*1000.0))/((double)m_CaptureDevice->get_InputSampleFrequency()));
-
   m_CaptureDevice->Destroy();
-
   return NULL;
+}
+
+void CSignalRecorder::StoreCapturedSamples(SndfileHandle &CaptureWave)
+{
+  ulong capturedSamples = 0;
+  do
+  {
+    capturedSamples = m_CaptureDevice->GetSamples(m_SamplesBuffer->get_Frame(0), m_FrameSize*m_MaxCaptureChannels);
+    capturedSamples /= m_MaxCaptureChannels;
+    if(capturedSamples > 0)
+    {
+      ulong writtenSamples = 0;
+      if(m_CaptureDevice->get_InputChannelAmount() > 1)
+      {
+        writtenSamples = (ulong)CaptureWave.writef(m_SamplesBuffer->get_Frame(0), capturedSamples);
+      }
+      else
+      {
+        writtenSamples = (ulong)CaptureWave.write(m_SamplesBuffer->get_Frame(0), capturedSamples);
+      }
+
+      if(writtenSamples < capturedSamples)
+      {
+        KODI->Log(LOG_ERROR, "Failed to write to capture wave file!");
+        Set_State(STATE_INVALID);
+      }
+    }
+  }while(capturedSamples > 0);
 }
--- src/RoomCorrection/SignalRecorder.h
+++ src/RoomCorrection/SignalRecorder.h
@@ -3,26 +3,55 @@
 #include <template/include/ADSPAddonMain.h>
 #include <kodi/threads/threads.h>
 #include "RoomCorrection/CaptureDevice/Source/PortAudioSource.h"
+#include <asplib/asplib_utils/buffers/TFrameBuffer.h>
+#include <sndfile/sndfile.hh>
+
+#define DEFAULT_FRAME_SIZE  2048
+typedef asplib::TFrameBuffer<float> CFloatFrameBuffer;
 
 class CSignalRecorder : public PLATFORM::CThread
 {
 public:
-  CSignalRecorder();
-  ~CSignalRecorder();
-	bool Create(uint SampleFrequency, std::string CaptureDevice = "");
-	bool Destroy();
+  typedef enum RecorderState
+  {
+    STATE_INVALID = -1,
 
-	bool StartRecording();
-	bool StopRecording();
+    STATE_IDLE,
+    STATE_CAPTURING,
+    STATE_NEW_FILE,
+    STATE_STOP,
+    STATE_FINISH_RECORDING,
+
+    STATE_MAX
+  }RecorderState;
+
+  CSignalRecorder(std::string PrefixStr="");
+  ~CSignalRecorder();
+  bool Create(uint SampleFrequency, uint MaxCaptureChannels, std::string CaptureDevice = "");
+  bool Destroy();
+
+  bool StartRecording(std::string PostFixStr="");
+  bool StopRecording();
+  bool FinishRecording();
 
   unsigned int Get_AvailableDevices(CCaptureDeviceList_t &DeviceList);
   unsigned int Get_AvailableDevices(CCaptureDeviceList_t &DeviceList, std::vector<uint> &SampleFrequencies);
 
-private:
-	virtual void *Process(void);
-	ulong ProcessSamples(uint8_t *Data, unsigned int Frames);
+  RecorderState Get_CurrentState();
 
-	volatile bool m_bStop;
-	PortAudioSource *m_CaptureDevice;
-	float m_Samples[2048];
+private:
+  void Set_State(RecorderState State);
+  virtual void *Process(void);
+  void StoreCapturedSamples(SndfileHandle &CaptureWave);
+
+  PortAudioSource   *m_CaptureDevice;
+  CFloatFrameBuffer *m_SamplesBuffer;
+  SndfileHandle     *m_CaptureWave;
+  uint32_t          m_FrameSize;
+  uint32_t          m_MaxCaptureChannels;
+  
+  RecorderState     m_State;
+  std::string       m_PostFixStr;
+  std::string       m_PrefixStr;
+  PLATFORM::CMutex  m_CriticalState;
 };
